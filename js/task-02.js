5604a36379d6fae16345b120fd033498143559fd
Viacheslav Haber <gaberslavik@gmail.com>
2022-10-22 00:42:41 +0300

    finished 2 task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	'Potatoes',
+	'Mushrooms',
+	'Garlic',
+	'Tomatos',
+	'Herbs',
+	'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+const elements = ingredients.map(item => {
+	const listItem = document.createElement('li');
+	listItem.textContent = item;
+	listItem.classList.add('item');
+	return listItem;
+});
+listEl.append(...elements);
+console.log(listEl);
