a3933b36cff55899cd93afca82713c415401ab97
Viacheslav Haber <gaberslavik@gmail.com>
2022-10-22 19:18:15 +0300

    finished 6-task

--- js/task-05.js
+++ js/task-05.js
@@ -5,9 +5,15 @@ const refs = {
 refs.inputRef.addEventListener('input', onInputChangeOutput);
 
 function onInputChangeOutput(event) {
-	if (!event.currentTarget.value) {
-		refs.outputLabelRef.textContent = 'Anonymous';
-	} else {
-		refs.outputLabelRef.textContent = event.currentTarget.value;
-	}
+	!event.currentTarget.value
+		? (refs.outputLabelRef.textContent = 'Anonymous')
+		: (refs.outputLabelRef.textContent = event.currentTarget.value);
 }
+
+// function onInputChangeOutput(event) {
+// 	if (!event.currentTarget.value) {
+// 		refs.outputLabelRef.textContent = 'Anonymous';
+// 	} else {
+// 		refs.outputLabelRef.textContent = event.currentTarget.value;
+// 	}
+// }
