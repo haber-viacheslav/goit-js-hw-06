25881bce043cda28e740941ddb719016bcc6a17c
Viacheslav Haber <gaberslavik@gmail.com>
2022-10-22 21:40:35 +0300

    finished 8-task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(evt) {
+	const formData = {};
+	evt.preventDefault();
+	const {
+		elements: { email, password },
+	} = evt.currentTarget;
+	// console.log(email.name);
+	// console.log(password.name);
+
+	if (email.value === '' || password.value === '') {
+		return alert('All fields must be filled');
+	}
+
+	formData[email.name] = email.value;
+	formData[password.name] = password.value;
+	console.log(formData);
+
+	evt.currentTarget.reset();
+}
